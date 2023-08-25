890fd689ed3b9296231e73aff9d142489cffcaaa
darko2019 <darko.ilic83@gmail.com>
2023-08-25 19:13:30 +0200

    Ubaceni novi fajovi u dps

--- dps-sajt/scripts/main.js
+++ dps-sajt/scripts/main.js
@@ -20,3 +20,28 @@ $(document).ready(function () {
         return false;
     });
 });
+
+// Toast za poslatu poruku u kontakt formi
+const toastMsg = document.querySelector('.toast')
+
+function toast() {
+    toastMsg.classList.replace('hide', 'show')
+    setTimeout(() => {
+        toastMsg.classList.replace('show', 'hide')
+
+    }, 1200);
+}
+
+// Reset kontakt forme i modal kontakt forme
+const kontaktForma = document.querySelector("#form-contact")
+const kontaktFormaBtn = document.querySelector('.send-dugme')
+if (kontaktForma) {
+    kontaktForma.addEventListener('submit', toast)
+    kontaktForma.addEventListener('submit', resetForm)
+}
+
+function resetForm() {
+    setTimeout(() => {
+        kontaktForma.reset();
+    }, 1000);
+}
