d6b6013860eeac3274421e9244d770d4188d841e
darko2019 <darko.ilic83@gmail.com>
2023-09-17 19:57:42 +0200

    Uklonjen JQ link u headu na svim stranicama. U JS promenjen kod za reset kontakt forme i back to top

--- scripts/main.js
+++ scripts/main.js
@@ -18,45 +18,47 @@ if (dropDownMenu) {
         });
     });
 }
-/*back to top*/
-$("#back-to-top").hide();
-$(document).ready(function () {
-    $(window).scroll(function () {
-        // console.log($(this).scrollTop());
-        if ($(this).scrollTop() > 150) {
-            $("#back-to-top").fadeIn();
+// SCROLL TO TOP
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const button = document.querySelector('#back-to-top');
+
+    button.addEventListener('click', function () {
+        window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+    });
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY < 200) {
+            button.style.opacity = "0";
         } else {
-            $("#back-to-top").fadeOut();
+            button.style.opacity = "1";
         }
     });
-    // scroll body to 0px on click
-    $("#back-to-top").click(function () {
-        $("body,html").animate(
-            {
-                scrollTop: 0,
-            },
-            400
-        );
-        return false;
-    });
+
 });
 // Toast za poslatu poruku u kontakt formi
-// namesteno je na click zbog testa na lokalu u produkciji promeniti na submit
-$(document).ready(function () {
-    $("#form-contact, #form-contact-modal").submit(function () {
-        $(".toast").toast({ delay: 1200 });
-        $(".toast").toast("show");
-        resetForm();
-    });
-});
+const toastMsg = document.querySelector('.toast')
+
+function toast() {
+    toastMsg.classList.replace('hide', 'show')
+    setTimeout(() => {
+        toastMsg.classList.replace('show', 'hide')
+
+    }, 2000);
+}
+
 // Reset kontakt forme i modal kontakt forme
+const kontaktForma = document.querySelector("#form-contact")
+const kontaktFormaBtn = document.querySelector('.send-dugme')
+if (kontaktForma) {
+    kontaktForma.addEventListener('submit', toast)
+    kontaktForma.addEventListener('submit', resetForm)
+}
+
 function resetForm() {
     setTimeout(() => {
-        $("#form-contact")[0].reset();
-        $("#form-contact-modal")[0].reset();
+        kontaktForma.reset();
     }, 1000);
-
-    console.log("resetovao sam formu");
 }
 
 const windowWidth = window.innerWidth;
