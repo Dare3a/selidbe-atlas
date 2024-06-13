2e440fb3a394466240e83ee7575540bd3a7b3cfe
darko2019 <darko.ilic83@gmail.com>
2024-06-13 14:19:38 +0200

    U php ubacen email office@selidbeatlas

--- kontakt.php
+++ kontakt.php
@@ -6,7 +6,7 @@ if (isset($_POST["submit"])) {
     $message = $_POST['message'];
 
 
-    $to = 'darko.ilic83@gmail.com';
+    $to = 'office@selidbeatlas.rs';
     $subject = 'Contact';
     $body = "Name: $name\r\n\nSurname: $surname\n\nE-mail: $email\n\nMessage: $message";
 
